2cd1ebd022f3e1141ab5369f4dd05e072bd5f049
Dustin <dc.christianson@gmail.com>
2022-06-19 09:21:53 -0600

    updated home page detail description

--- pages/index.js
+++ pages/index.js
@@ -26,19 +26,30 @@ export default function Home({ allPostsData }) {
       <div className={utilStyles.navList}>
       <Link href="/blogs"><a>Blog</a></Link>
       <Link href="/pages/projects"><a>Projects</a></Link>
-      <Link href="/pages/about"><a>About</a></Link>
+      <a href="https://github.com/uncleBlobby">Github</a>
       </div>       
-        <p>My name is Dustin Christianson and I am studying software development.</p>
-        <p>My current interests include:</p>
+      </section>
+      <section>
+        <p>My name is Dustin Christianson and I am studying software development.  I use this page to catalogue some of my work and share my thoughts.  Thanks for stopping by!</p>
+        <p>Some of my current interests include:</p>
         <ul>
           <li>React Frontend (ViteJS, NextJS in particular)</li>
           <li>ExpressJS Backend</li>
           <li>Sqlite3 RDMS, SQL</li>
           <li>Python, Go, and C</li>
+          <li>Linux, FOSS</li>
         </ul>
-        <p>
-          This is a simple website I use to catalogue my work and to share my thoughts.  Thanks for stopping by, I really hope you enjoy your stay.
-        </p>
+        <p>Outside of software development, I have a variety of other interests.  I hold a B.A. in Philosophy from the University of Regina, and I 
+        am currently doing independent research in the field of games-based learning, reading <a href="https://en.wikipedia.org/wiki/Johan_Huizinga">Huizinga</a> and 
+        <a href="https://en.wikipedia.org/wiki/Mihaly_Csikszentmihalyi"> Csikszentmihalyi</a>, trying to tie some of their work back into <a href="https://en.wikipedia.org/wiki/Hans-Georg_Gadamer">Gadamer's</a> theory of play 
+        from his magnum opus <i><a href="Truth and Method">Truth and Method</a></i>. Don't worry, I can't pronounce the guy's name either.</p>
+        <p>On top of it all, I work full time in the craft beverage industry and I am currently studying Computer Science part - time at the University of Regina.</p>
+        <p>In my free time, I like to play video games--and I would really like to eventually create one of my own.  I have had a few false starts in that regard, but 
+        learning to work with video game engines (<a href="https://godotengine.org/">Godot</a>, in particular) has been a fun experience, and helpful in terms of applying some of the things I've been learning about programming.</p>
+        <p>I've spent almost 20 years learning to play guitar, and more recently took it on myself to start trying to sing as well.  Also more recently, I've inherited
+        my grandfather's old accordion--but I've still got a long way to go before I can play it with any sort of confidence.  <p>Apologies to my neighbours.</p>  I have released a few electronic
+        music EP's, started a rap album I have yet to finish, worked on post-production audio for several short films, and recorded almost 50 podcast episodes with some of my good pals.</p>
+        <p>&#128075; Bye for now! &#128075;</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog Posts</h2>
@@ -49,7 +60,7 @@ export default function Home({ allPostsData }) {
                 <a>{title}</a>
               </Link>
               <br />
-              <small className={utilStyles.lightText}>
+              <small className={utilStyles.darkText}>
                 <Date dateString={date} />
               </small>
             </li>
